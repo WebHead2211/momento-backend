0e0dcd18680171ee01c008de24a101da617d9cd8
Aayush Bakre <29601578+WebHead2211@users.noreply.github.com>
2024-11-06 11:49:16 +0530

    connect to mongodb atlas

--- src/index.js
+++ src/index.js
@@ -1,29 +1,4 @@
-import 'dotenv/config'
-import express from "express";
-const app = express();
+import "dotenv/config";
+import connectDb from "./db/index.js";
 
-app.get("/", (req, res) => {
-  res.send("hello world");
-});
-
-app.get("/api/jokes", (req, res) => {
-  const jokes = [
-    {
-      id: 1,
-      title: "A joke",
-      content: "Joke number 1",
-    },
-    {
-      id: 2,
-      title: "Another joke",
-      content: "Joke number 2",
-    },
-  ];
-  res.send(jokes);
-});
-
-const port = process.env.PORT || 3000;
-
-app.listen(port, () => {
-  console.log(`Running on port ${port}`);
-});
+connectDb();
